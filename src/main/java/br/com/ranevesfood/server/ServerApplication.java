e487af4c545de190c01e2bec67786bd2c703ab99
javaExactus <romulo.neves@exactus.com.br>
2024-11-25 14:25:45 -0300

    ajustes

--- src/main/java/br/com/ranevesfood/server/ServerApplication.java
+++ src/main/java/br/com/ranevesfood/server/ServerApplication.java
@@ -1,4 +1,4 @@
-package br.com.romulo.server;
+package br.com.ranevesfood.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/br/com/ranevesfood/server/ServerApplicationTests.java
+++ src/test/java/br/com/ranevesfood/server/ServerApplicationTests.java
@@ -1,4 +1,4 @@
-package br.com.romulo.server;
+package br.com.ranevesfood.server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
